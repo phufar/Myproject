7159e25971c286384af985dfa68da9f7896ab0ba
phufar <98950819+phufar@users.noreply.github.com>
2022-05-19 14:13:41 +0700

    Update autoclicker.py

--- autoclicker/autoclicker.py
+++ autoclicker/autoclicker.py
@@ -1,9 +1,5 @@
-
 #create auto clicker
-
-import time
-import pyautogui
-import keyboard
+import time, pyautogui, keyboard
 i = 1
 def click():
     time.sleep(0.1)
@@ -13,11 +9,11 @@ def main():
     global i
     while i == 1:
         click()
-        if keyboard.is_pressed('q'):
+        if keyboard.is_pressed('q'):#stop
             time.sleep(1)
             i = 0
             break	
 while True:
-    if keyboard.is_pressed('a'):
+    if keyboard.is_pressed('a'):#start
         i = 1
         main()
